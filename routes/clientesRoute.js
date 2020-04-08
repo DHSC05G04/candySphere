93dac243e81d822de9fad366d6c8e0317b54240b
Âlessandro <asovitorio@gmail.com>
2020-04-08 18:53:02 -0300

    Atualização da tabela cliente

--- routes/clientesRoute.js
+++ routes/clientesRoute.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+let clienteController = require('../controllers/clienteController');
 /* GET clientes page. */
-router.get('/', function(req, res, next) {
-  let tabActive = {homeAct: "inactive",
-                  adminAct: "inactive",
-                  financeiroAct: "inactive",
-                  clientesAct: "active",
-                  funcionariosAct: "inactive",
-                  pdvAct: "inactive"};
-  res.render('clientes', { title: 'Express', tabs: tabActive });
-});
+router.get('/',clienteController.listarCliente)
+router.get('/cadastrarCliente',(req,res)=>{
+    res.send("<h1>deu certo</h1>")
+})
 
 module.exports = router;
