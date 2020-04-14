24e27e8999d8941e48acc03cfa1d3b98efc555e1
rastapf <rupertstapf@hotmail.com>
2020-04-14 11:12:32 -0300

    Inclusão da página de funcionários e início do modal do carrinho de compras

---
+++ controllers/funcionarioController.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const funcionarioController = {
+    listarFuncionario: (req, res, next) => {
+        let tabActive = {
+            homeAct: "inactive",
+            adminAct: "inactive",
+            financeiroAct: "inactive",
+            clientesAct: "inactive",
+            funcionariosAct: "active",
+            pdvAct: "inactive"
+        };
+
+        const dataFuncionario = JSON.parse(
+            fs.readFileSync(
+                path.join('database', 'funcionarios.json')));
+                res.render('funcionarios', {
+            title: 'Express',
+            tabs: tabActive,
+            funcionarios: dataFuncionario
+        });
+    }
+
+}
+
+module.exports = funcionarioController;
