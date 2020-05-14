669a4f7ab12c313a2e78f141c066e0c07fcb8497
Leonardo Souza <llsouza.br@gmail.com>
2020-05-14 01:41:36 -0300

    Creating Migrations

---
+++ migrations/20200514042012-create_table_pedidos.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.createTable(
+     'users',{
+       id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          unique: true,
+        },
+        dt_entrada: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          default: new Date(),
+        },
+        dt_entrega: {
+          type: Sequelize.DATE,
+          allowNull: true,
+        },
+        vlr_total: {
+          type: Sequelize.DECIMAL(11,2),
+          default: 0.00,
+        },
+        vlr_sinal: {
+          type: Sequelize.DECIMAL(11,2),
+          default: 0.00,
+        },
+        status_id: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Status',
+            key: 'id'
+          },
+        },
+        observacao:{
+          type: Sequelize.STRING(1024),
+          allowNull: true,
+        },
+        created_at: {
+          type:Sequelize.DATE,
+          default: new Date(),
+        },
+        updated_at: {
+          type:Sequelize.DATE,
+          default: new Date(),
+        },
+        // Paranoid Mode
+        deleted_at: {
+          type:Sequelize.DATE,
+          allowNull: true
+        },
+
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('pedidos');
+  }
+};
