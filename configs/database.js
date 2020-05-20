73c552858c48284aca7ecd20e9c4239bdd8ba12e
rastapf <rupertstapf@hotmail.com>
2020-05-20 01:24:18 -0300

    Created db config file using env variables

---
+++ configs/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const config = {
+    database: 'candySphere',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    timestamps: false
+}
+
+module.exports = config;
