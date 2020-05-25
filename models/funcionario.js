6175e7bc100e1f9571150d31d19dbadbcb1e334f
rastapf <rupertstapf@hotmail.com>
2020-05-25 19:54:21 -0300

    Alteração nos models e provisão para uso de timestamps pelo sequelize

--- models/funcionario.js
+++ models/funcionario.js
@@ -1,15 +1,39 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const funcionario = sequelize.define('funcionario', {
-    id: DataTypes.INTEGER,
-    nome: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: DataTypes.STRING,
     avatar: DataTypes.STRING,
     endereco: DataTypes.STRING,
     telefone: DataTypes.STRING,
-    salario: DataTypes.DECIMAL,
-    data_criacao: DataTypes.DATE,
-    data_modificacao: DataTypes.DATE
+    salario: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at'
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: 'deleted_at'      
+    }
   }, {});
   funcionario.associate = function(models) {
     // associations can be defined here
--- models/usuario.js
+++ models/usuario.js
@@ -1,13 +1,37 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const usuario = sequelize.define('usuario', {
-    id: DataTypes.INTEGER,
-    nome_usuario: DataTypes.STRING,
-    senha: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    nome_usuario: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     acesso: DataTypes.INTEGER,
-    funcionario_id: DataTypes.INTEGER,
-    data_criacao: DataTypes.DATE,
-    data_modificacao: DataTypes.DATE
+    funcionario_id: DataTypes.INTEGER.UNSIGNED,
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'created_at'
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: 'updated_at'
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: 'deleted_at'      
+    }
   }, {});
   usuario.associate = function(models) {
     // associations can be defined here
