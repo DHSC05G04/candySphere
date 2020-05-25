427fddadce618575143d8ae0a55fcc76764d3156
rastapf <rupertstapf@hotmail.com>
2020-05-25 20:17:07 -0300

    Adição dos migrations do Alessandro ao arquivo principal

--- migrations/20200518195134-candysphere_start.js
+++ migrations/20200518195134-candysphere_start.js
@@ -373,20 +373,369 @@ module.exports = {
     });
     
     /* Incluir tabela Terminais */
+    await queryInterface.createTable('terminais', {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      descricao: Sequelize.STRING,
+
+      pedido_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'pedidos'
+          },
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela Caixa */
+    await queryInterface.createTable('caixas', { 
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      hora_abertura: Sequelize.DATE,
+      hora_fechamento: Sequelize.DATE,
+      terminal_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'terminais'
+          },
+          key:'id'
+        }
+      },
+      usuario_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'usuarios'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela Pedidos */
+    await queryInterface.createTable('pedidos', { 
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      entrada: Sequelize.DATE,
+      entrega: Sequelize.DATE,
+      entrega: Sequelize.DATE,
+      total: Sequelize.DECIMAL,
+      sinal: Sequelize.DECIMAL,
+      obervacao: Sequelize.STRING,
+      
+      status_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'status'
+          },
+          key:'id'
+        }
+      },
+      caixa_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'caixas'
+          },
+          key:'id'
+        }
+      },
+      cliente_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'clientes'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela Comandas */
+    await queryInterface.createTable('comandas', { 
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nota_fiscal: Sequelize.STRING,
+      
+      pedido_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'pedidos'
+          },
+          key:'id'
+        }
+      },
+      produto_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'produtos'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela Recebimentos */
+    await queryInterface.createTable('recebimentos', {
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      valor: Sequelize.DECIMAL,
+      aprovado: Sequelize.BINARY,
+      
+      pedido_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'pedidos'
+          },
+          key:'id'
+        }
+      },
+      caixa_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'caixas'
+          },
+          key:'id'
+        }
+      },
+      forma_pagamento_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'forma_pagamentos'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+
+    });
 
     /* Incluir tabela Contas */
+    await queryInterface.createTable('contas', {
+      id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      valor: Sequelize.DECIMAL,
+      aprovado: Sequelize.BINARY,
+      usuarios_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'usuarios'
+          },
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela Pagamentos */
+    queryInterface.createTable('pagamentos', { 
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      data_pgto: Sequelize.DATE,
+      valor: Sequelize.DECIMAL,
+      obervacao: Sequelize.STRING,
+      
+      status_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'status'
+          },
+          key:'id'
+        }
+      },
+      conta_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'contas'
+          },
+          key:'id'
+        }
+      },
+      formas_pagamento_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'formas_pagamentos'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     /* Incluir tabela conta_movimento */
+    await queryInterface.createTable('conta_movimentos', { 
+      id:{
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      data: Sequelize.DATE,
+      saldo: Sequelize.DECIMAL,
+      obervacao: Sequelize.STRING,
+      
+      recebimentos_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'recebimentos'
+          },
+          key:'id'
+        }
+      },
+      pagamento_id:{
+        type:Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+        references:{
+          model:{
+            tableName:'pagamentos'
+          },
+          key:'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
 
     return console.log('Banco de dados CandySphere criado corretamente!')
 
