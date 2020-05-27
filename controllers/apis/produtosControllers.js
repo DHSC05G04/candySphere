fa55b531b622bb4caa56f1176ec0b034a83c93bd
Âlessandro <asovitorio@gmail.com>
2020-05-27 13:47:04 -0300

    controller produto criado

---
+++ controllers/apis/produtosControllers.js
@@ -0,0 +1,19 @@
+const produtoController = {
+    index: (req, res) => {
+
+    },
+
+    store: (req, res) => {
+
+    },
+
+    update: (req, res) => {
+
+    },
+
+    delete: (req, res) => {
+
+        }
+};
+
+module.exports = estocaveisController;
