877080c7dbac91aeaf80bbffced980bd50ea85ac
rastapf <rupertstapf@hotmail.com>
2020-05-29 04:52:59 -0300

    Criação da rota de usuários

--- routes/apis/usuariosRoute.js
+++ routes/apis/usuariosRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-//const usuariosController = require('../../controllers/apis/usuariosController');
+const usuariosController = require('../../controllers/apis/usuariosController');
 
 const router = express.Router();
 
-
+router.get('/', usuariosControllers.index);
+router.get('/:id', usuariosControllers.index);
+router.post('/', usuariosControllers.create);
+router.put('/', usuariosControllers.update);
+router.put('/:id', usuariosControllers.update);
+router.delete('/', usuariosControllers.delete);
+router.delete('/:id', usuariosControllers.delete);
 
 module.exports = router;
