6c9ea7f81e5c97ccac5f917330d6f1492989e4c0
rastapf <rupertstapf@hotmail.com>
2020-05-29 04:08:41 -0300

    Inicialização de rota e controller da entidade produtos

--- routes/apis/produtosRoute.js
+++ routes/apis/produtosRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-constTerminaisController = require('../../controllers/apis/pedidosControllers');
+const produtosController = require('../../controllers/apis/produtosController');
 
 const router = express.Router();
 
-
+router.get('/', produtosController.index);
+router.get('/:id', produtosController.index);
+router.post('/', produtosController.create);
+router.put('/', produtosController.update);
+router.put('/:id', produtosController.update);
+router.delete('/', produtosController.delete);
+router.delete('/:id', produtosController.delete);
 
 module.exports = router;
