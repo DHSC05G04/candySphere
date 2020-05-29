c04ccc58d9788c92c4c714c585965f73259799be
rastapf <rupertstapf@hotmail.com>
2020-05-29 02:45:34 -0300

    Inicialização das rotas e controllers para CRUD da entidade ingredientes

---
+++ controllers/apis/ingredientesController.js
@@ -0,0 +1,9 @@
+const { Op } = require('sequelize');
+
+const { Ingrediente, sequelize } = require('../../models');
+
+const ingredientesController = {
+
+}
+
+module.exports = ingredientesController;
