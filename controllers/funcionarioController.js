48194b8e6f3390f2ad49367dc48fd0bd60a5cf0d
Alessandro <asovitorio@gmail.com>
2020-06-04 10:11:07 -0300

    front funcionario-usuario

--- controllers/funcionarioController.js
+++ controllers/funcionarioController.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-
+ const fetch = require('node-fetch');
+const API_BASE ='http://localhost:3000/api/v0';
 const funcionarioController = {
-    listarFuncionario: (req, res, next) => {
+    listarFuncionario: async (req, res, next) => {
         let tabActive = {
             homeAct: "inactive",
             adminAct: "inactive",
@@ -12,17 +13,29 @@ const funcionarioController = {
             pdvAct: "inactive"
         };
 
-        const dataFuncionario = JSON.parse(
-            fs.readFileSync(
-                path.join('database', 'funcionarios.json')));
-                res.render('funcionarios', {
-            title: 'Express',
-            tabs: tabActive,
-            funcionarios: dataFuncionario,
-            usuario:req.session.user
-        });
+        // const dataFuncionario = JSON.parse(
+        //     fs.readFileSync(
+        //         path.join('database', 'funcionarios.json')));
+
+        try {
+            const funcionarioAPI = await fetch(`${API_BASE}/funcionarios`);
+            const funcionarios = await funcionarioAPI.json();
+           
+            
+         
+           return res.render('funcionarios', {
+                title: 'Express',
+                tabs: tabActive,
+                funcionarios: funcionarios,
+                usuario:req.session.user
+            });
+            
+        } catch (error) {
+            return res.send(error)            
+        }
+         
     },
-    verFuncionario: (req, res, next) => {
+    verFuncionario: async (req, res, next) => {
         let tabActive = {
             homeAct: "inactive",
             adminAct: "inactive",
@@ -31,16 +44,30 @@ const funcionarioController = {
             funcionariosAct: "active",
             pdvAct: "inactive"
         };
-
-        const dataFuncionario = JSON.parse(
-            fs.readFileSync(
-                path.join('database', 'funcionarios.json')));
-                res.render('funcionario/funcionarioView', {
-            title: 'Express',
-            tabs: tabActive,
-            funcionarios: dataFuncionario,
-            usuario:req.session.user
-        });
+        try {
+            
+            const {id} = req.params
+            const funcionarioAPI = await fetch(`${API_BASE}/funcionarios/${id}`);
+                const funcionario = await funcionarioAPI.json();
+                const usuarioAPI = await fetch(`${API_BASE}/usuarios/${id}`);
+                const usuarioCadastrado = await usuarioAPI.json();
+                    if(usuarioCadastrado.length <=0){
+                        console.log("não tem cadastro")
+                    }else{
+                        console.log(usuarioCadastrado)
+                    }
+                   
+                    res.render('funcionario/funcionarioView', {
+                title: 'Express',
+                tabs: tabActive,
+                funcionarios: funcionario,
+                usuario:req.session.user,
+                usuarioCadastrado
+            });
+        } catch (error) {
+            return res.send(error)
+        }
+       
     },
 
 }
