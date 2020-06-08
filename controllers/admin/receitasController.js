a1bfd11f367aa2d9591cbfdb3f6fd3e2a912f621
Rupert A Stapf <rupertstapf@hotmail.com>
2020-06-08 04:25:17 -0300

    Criação de um controller específico para receitas

---
+++ controllers/admin/receitasController.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+const API_BASE = 'http://localhost:3000/api/v0';
+
+const receitasController = {
+    index: (req, res) => {
+
+    },
+    store: (req, res) => {
+
+    }
+}
+
+module.exports = receitasController;
