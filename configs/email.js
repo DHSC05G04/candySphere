18a849f624b0c552986ea9c7d40f4d0c25acad39
Leonardo Souza <llsouza.br@gmail.com>
2020-06-13 20:26:12 -0300

    Retirado senha

--- configs/email.js
+++ configs/email.js
@@ -3,9 +3,7 @@ const nodemailer = require('nodemailer');
 const email = nodemailer.createTransport({
     service:'gmail',
     auth:{
-        
-        // user:'dhsc.candysphere@gmail.com',
-        // pass:'santandercoders240'
+       
         user:`${process.env.EMAIL_USER}`,
         pass:`${process.env.EMAIL_SENHA}`
     }
