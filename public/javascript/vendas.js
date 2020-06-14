6b6e97e473b7c213c5f3a458a195f3008fe71141
Rupert A Stapf <rupertstapf@hotmail.com>
2020-06-14 12:08:16 -0300

    Visual da tela PDV finalizado

--- public/javascript/vendas.js
+++ public/javascript/vendas.js
@@ -1,3 +1,5 @@
+const API_BASE = 'http://localhost:3000/api/v0/produtos'
+
 function addToCart(productId) {
     if(!sessionStorage.carrinho) {
         return sessionStorage.carrinho = JSON.stringify([{id:productId, qtd: 1}])
@@ -17,30 +19,34 @@ function addToCart(productId) {
 function iniciaModal(modalId){
     const tabelaCompras = document.getElementById('listaCompras')
     const produtosCarrinho = JSON.parse(sessionStorage.carrinho);
-    console.log(produtosCarrinho)
-    const API_BASE = 'http://localhost:3000/api/v0/produtos'
 
-    produtosCarrinho.forEach(async produto => {
-        const dados = await fetch(`${API_BASE}/${produto.id}`)
-        const dadosObject = await dados.json()
-        console.log(dadosObject)
-        tabelaCompras.innerHTML += `
-            <tr>
-                <td>${dadosObject[0].itemEstoque.nome}</td>
-                <td>${produto.qtd}</td>
-                <td>${dadosObject[0].valor}</td>
+    produtosCarrinho.forEach(async (produto, index) => {
+        const dadosAPI = await fetch(`${API_BASE}/${produto.id}`)
+        const dados = await dadosAPI.json()
+
+        if(index == 0) {
+            tabelaCompras.innerHTML = `
+            <tr class="itensCarrinho" id=${produto.id}>
+                <td>${dados[0].itemEstoque.nome}</td>
+                <td><input type="number" pattern="0" step="1" value="${produto.qtd}"></td>
+                <td>${dados[0].valor}</td>
+            </tr>
+            `
+        } else {
+            tabelaCompras.innerHTML += `
+            <tr class="itensCarrinho" id=${produto.id}>
+                <td>${dados[0].itemEstoque.nome}</td>
+                <td><input type="number" pattern="0" step="1" value="${produto.qtd}"></td>
+                <td>${dados[0].valor}</td>
             </tr>
         `
+        }
     })
-
-
-
-
 
     modalId.classList.add('mostrar');
     modalId.addEventListener('click',(e)=>{
-    if(e.target.id =='fechar-tudo'){
-        modal.classList.remove('mostrar');
-    };
+        if(e.target.id =='fechar-tudo'){
+            modalId.classList.remove('mostrar');
+        };
     });
 };
